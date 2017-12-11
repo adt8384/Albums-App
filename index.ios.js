0cd0e6ac7e31571f62d35250d6ae20f7a114b725
Aditi Deshpande <adt8384@gmail.com>
2017-12-10 20:20:02 -0800

    first commit

---
+++ index.ios.js
@@ -0,0 +1,16 @@
+// import some library
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// create a component
+const App = () => (
+  <View style= {{ flex: 1}}>
+    <Header headerText= {'Albums'}/>
+    <AlbumList />
+  </View>
+ );
+
+//render component to device
+AppRegistry.registerComponent('albums', () => App);
